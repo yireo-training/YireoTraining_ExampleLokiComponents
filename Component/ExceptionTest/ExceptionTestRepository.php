b9851d7ac17866959c1ce701665da4bd1c55ccfc
Jisse Reitsma <jisse@yireo.com>
2025-05-26 15:08:33 +0200

    Example with exception handling

---
+++ Component/ExceptionTest/ExceptionTestRepository.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace YireoTraining\ExampleLokiComponents\Component\ExceptionTest;
+
+use RuntimeException;
+use Yireo\LokiComponents\Component\ComponentRepository;
+
+class ExceptionTestRepository extends ComponentRepository
+{
+    public function getValue(): mixed
+    {
+        return '';
+    }
+
+    public function saveValue($value): void
+    {
+        throw new RuntimeException('This exception was thrown in the ExceptionTestRepository');
+    }
+}
---
+++ Controller/Example/ExceptionTest.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace YireoTraining\ExampleLokiComponents\Controller\Example;
+
+class ExceptionTest extends Index
+{
+}
